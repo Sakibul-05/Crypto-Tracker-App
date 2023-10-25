60e9d9db75cc56da108d4e22f71c8b66a949252f
Sakibul <sksakibulislam30@gmail.com>
2023-10-26 01:44:36 +0530

    update

--- src/pages/WatchlistPage.js
+++ src/pages/WatchlistPage.js
@@ -1,9 +1,73 @@
+// import React, { useEffect, useState } from "react";
+// import Header from "../components/Common/Header";
+// import Loader from "../components/Common/Loader";
+// import TabsComponent from "../components/Dashboard/Tabs";
+// import { get100Coins } from "../functions/get100Coins";
+// import BasicButton from "../components/Common/Button";
+// import { Link } from "react-router-dom";
+
+// function WatchlistPage() {
+//   const coins = JSON.parse(localStorage.getItem("watchlist")) || [];
+//   const [myWatchlist, setMyWatchlist] = useState([]);
+//   const [loading, setLoading] = useState(false);
+
+//   useEffect(() => {
+//     getData();
+//   },[]);
+
+//   const getData = async () => {
+//     setLoading(true);
+//     const allCoins = await get100Coins();
+//     if (coins) {
+//       setMyWatchlist(allCoins.filter((item) => coins.includes(item.id)));
+//     }
+//     setLoading(false);
+//   };
+
+//   return (
+//     <div>
+//       {loading || !coins ? (
+//         <Loader />
+//       ) : (
+//         <div style={{ minHeight: "90vh" }}>
+//           {myWatchlist?.length == 0 || !coins ? (
+//             <div>
+//               <Header />
+//               <h1 style={{ textAlign: "center", marginBottom: "2rem" }}>
+//                 No Items in the Watchlist
+//               </h1>
+//               <div style={{ display: "flex", justifyContent: "center" }}>
+//                 <Link to="/dashboard">
+//                 <BasicButton variant="contained" text="Dashboard"/>
+//                 </Link>
+//               </div>
+//             </div>
+//           ) : (
+//             <div style={{ height: "95vh" }}>
+//               <Header />
+//               <TabsComponent coins={myWatchlist} isWatchlistPage={true} />
+//             </div>
+//           )}
+          
+//         </div>
+//       )}
+//       {/* <Footer/> */}
+//     </div>
+   
+//   );
+ 
+// }
+
+// export default WatchlistPage;
+
+
 import React, { useEffect, useState } from "react";
 import Header from "../components/Common/Header";
 import Loader from "../components/Common/Loader";
 import TabsComponent from "../components/Dashboard/Tabs";
 import { get100Coins } from "../functions/get100Coins";
 import BasicButton from "../components/Common/Button";
+import { Link } from "react-router-dom";
 
 function WatchlistPage() {
   const coins = JSON.parse(localStorage.getItem("watchlist")) || [];
@@ -12,15 +76,20 @@ function WatchlistPage() {
 
   useEffect(() => {
     getData();
-  },[]);
+  }, []);
 
   const getData = async () => {
     setLoading(true);
-    const allCoins = await get100Coins();
-    if (coins) {
-      setMyWatchlist(allCoins.filter((item) => coins.includes(item.id)));
+    try {
+      const allCoins = await get100Coins();
+      if (coins && Array.isArray(allCoins)) {
+        setMyWatchlist(allCoins.filter((item) => coins.includes(item.id)));
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
     }
-    setLoading(false);
   };
 
   return (
@@ -29,16 +98,16 @@ function WatchlistPage() {
         <Loader />
       ) : (
         <div style={{ minHeight: "90vh" }}>
-          {myWatchlist?.length == 0 || !coins ? (
+          {myWatchlist?.length === 0 || !coins ? (
             <div>
               <Header />
               <h1 style={{ textAlign: "center", marginBottom: "2rem" }}>
                 No Items in the Watchlist
               </h1>
               <div style={{ display: "flex", justifyContent: "center" }}>
-                <a href="/dashboard">
-                <BasicButton variant="contained" text="Dashboard"/>
-                </a>
+                <Link to="/dashboard">
+                  <BasicButton variant="contained" text="Dashboard" />
+                </Link>
               </div>
             </div>
           ) : (
@@ -47,14 +116,11 @@ function WatchlistPage() {
               <TabsComponent coins={myWatchlist} isWatchlistPage={true} />
             </div>
           )}
-          
         </div>
       )}
-      {/* <Footer/> */}
     </div>
-   
   );
- 
 }
 
 export default WatchlistPage;
+
